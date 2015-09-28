46c8756b1a379a76131df2a7dd538f3dde156d51
saadriz <saad.h.rizvi@gmail.com>
2015-09-27 20:06:12 -0400

    changes

--- your-first-NSLog/FISAppDelegate.m
+++ your-first-NSLog/FISAppDelegate.m
@@ -6,6 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
+    NSLog(@"Hello, World!");
     /**
     
      * Hey! Write your code here! :D
